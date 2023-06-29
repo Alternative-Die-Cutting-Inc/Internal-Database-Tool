eca8a4cfdc6191a3cd6a686773199733e245c833
Farbod M <farbodm.dev@gmail.com>
2023-06-29 11:05:25 -0400

    delete local data

--- mongo-backend/models/QuoteModel.js
+++ mongo-backend/models/QuoteModel.js
@@ -1,6 +1,75 @@
 const mongoose = require("mongoose");
 
-const quoteJobSchema = new mongoose.Schema({});
+const extraChargeSchema = new mongoose.Schema({
+  chargeName: {
+    type: String,
+    required: true,
+  },
+  chargePerM: {
+    type: Number,
+    required: false,
+  },
+  chargeCost: {
+    type: Number,
+    required: false,
+  },
+});
+
+const quoteJobSchema = new mongoose.Schema({
+  units: {
+    type: Number,
+    required: false,
+  },
+  perSheet: {
+    type: Number,
+    required: false,
+  },
+  clientNotes: {
+    type: String,
+    required: false,
+  },
+  internalNotes: {
+    type: String,
+    required: false,
+  },
+  extraCharges: [extraChargeSchema],
+  dieHours: {
+    type: Number,
+    required: false,
+  },
+  dieSetup: {
+    type: Number,
+    required: false,
+  },
+  dieRunSpeed: {
+    type: Number,
+    required: false,
+  },
+  dieRunM: {
+    type: Number,
+    required: false,
+  },
+  gluerSetupHours: {
+    type: Number,
+    required: false,
+  },
+  gluerRunSpeed: {
+    type: Number,
+    required: false,
+  },
+  gluerRunM: {
+    type: Number,
+    required: false,
+  },
+  stripRunSpeed: {
+    type: Number,
+    required: false,
+  },
+  stripRunM: {
+    type: Number,
+    required: false,
+  },
+});
 
 const QuoteSchema = new mongoose.Schema({
   quoteNumber: {
@@ -27,7 +96,6 @@ const QuoteSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
-
   quoteJobs: [quoteJobSchema],
 });
 

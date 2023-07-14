4fe8c0db896ff1cfa95a7d8aad649e72125a800e
Farbod M <farbodm.dev@gmail.com>
2023-07-13 22:36:29 -0400

    Finish Docket tests

--- mongo-backend/test/dbconfig.test.js
+++ mongo-backend/test/dbconfig.test.js
@@ -1,27 +1,3 @@
-// const mongoose = require("mongoose");
-
-// // tells mongoose to use ES6 implementation of promises
-// mongoose.Promise = global.Promise;
-// require("dotenv").config();
-
-// const mongoURI = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@localhost:27017/testingDatabase?authSource=admin`;
-
-// console.log(mongoURI);
-// mongoose.connect(mongoURI);
-// mongoose.connection
-//   .once("open", () => console.log("Connected!"))
-//   .on("error", (error) => {
-//     console.warn("Error : ", error);
-//   });
-
-// // runs before each test
-// beforeEach(async (done) => {
-//   // drop the collection
-//   mongoose.connection.dropDatabase(() => {
-//     done();
-//   });
-// });
-
 const mongoose = require("mongoose");
 const mongoURI = `mongodb://admin:root@localhost:27017/testingDatabase?authSource=admin`;
 
--- mongo-backend/test/dockerServices.test.js
+++ mongo-backend/test/dockerServices.test.js
@@ -2,25 +2,29 @@ const docketServices = require("../services/docketServices");
 const assert = require("assert");
 
 describe("Testing Docket Services", () => {
+  it(".initCounter()\t\t|\t\tInitialize Counter", async function () {
+    await docketServices.initCounter();
+  });
+
   let newDocket = null;
-  it(".create()\t|\tCreate a Docket", async function () {
+  it(".create()\t\t\t|\t\tCreate a Docket", async function () {
     newDocket = await docketServices.create({
       customerName: "Alt Die Cut Inc",
       jobName: "Test 0",
     });
   });
 
-  it(".create()\t|\tAccurate Creation", async function () {
+  it(".create()\t\t\t|\t\tAccurate Creation", async function () {
     newDocket = await docketServices.create({
       customerName: "Alt Die Cut Inc",
       jobName: "Test 1",
     });
     assert.equal(newDocket.customerName, "Alt Die Cut Inc");
     assert.equal(newDocket.jobName, "Test 1");
-    assert.equal(newDocket.docketNumber, 2);
+    assert.equal(newDocket.docketNumber, 45002);
   });
 
-  it(".create()\t|\tCreate multiple Dockets", async function () {
+  it(".create()\t\t\t|\t\tCreate multiple Dockets", async function () {
     await docketServices.create({
       customerName: "Alt Die Cut Inc",
       jobName: "Test 2",
@@ -43,14 +47,62 @@ describe("Testing Docket Services", () => {
     });
   });
 
-  it(".get(id)\t|\tGet a Docket", async function () {
+  it(".get(id)\t\t\t|\t\tGet a Docket", async function () {
     const existingDocket = await docketServices.get(newDocket.id);
     assert.notEqual(existingDocket, null);
   });
 
-  it(".get()\t|\tGet all Dockets", async function () {
+  it(".get()\t\t\t|\t\tGet all Dockets", async function () {
     const existingDockets = await docketServices.get();
     assert.notEqual(existingDockets.length, 0);
     assert.equal(existingDockets.length, 7);
+  });
+
+  it(".update(id, fields)\t|\t\tUpdate Docket info", async function () {
+    const updatedDocket = await docketServices.update(newDocket.id, {
+      customerName: "Alternative Die Cut Inc",
+      jobName: "Test 7",
+      customerPO: 123456,
+    });
+    assert.equal(updatedDocket.customerName, "Alternative Die Cut Inc");
+    assert.equal(updatedDocket.jobName, "Test 7");
+    assert.equal(updatedDocket.customerPO, 123456);
+  });
+  it(".update(id, fields)\t|\t\tAdd forms", async function () {
+    let forms = [
+      {
+        formName: "Form 1",
+        formQuantity: 123,
+        quantityShipped: 123,
+      },
+      {
+        formName: "Form 2",
+        formQuantity: 246,
+        quantityShipped: 123,
+      },
+      {
+        formName: "Form 3",
+        formQuantity: 369,
+        quantityShipped: 123,
+      },
+    ];
+    const updatedDocket = await docketServices.update(newDocket.id, {
+      forms,
+    });
+    assert.equal(updatedDocket.forms.length, 3);
+    updatedDocket.forms.forEach((form, index) => {
+      assert.equal(form.formName, `Form ${index + 1}`);
+      assert.equal(form.formQuantity, 123 * (index + 1));
+      assert.equal(form.quantityShipped, 123);
+    });
+  });
+
+  it(".delete(id)\t\t|\t\tDelete a Docket", async function () {
+    const deletedDocket = await docketServices.delete(newDocket.id);
+    assert.notEqual(deletedDocket, null);
+  });
+  it(".delete(id)\t\t|\t\tCheck deleted Docket", async function () {
+    const deletedDocket = await docketServices.delete(newDocket.id);
+    assert.equal(deletedDocket, null);
   });
 });

8ee7fa71df50f1877a1c0a9f599b66c2050489c4
Freeassassin <farbodemzadeh@gmail.com>
2023-07-25 11:23:55 -0400

    add customer router to routerLoader

--- backend/loaders/routerLoader.js
+++ backend/loaders/routerLoader.js
@@ -1,16 +1,18 @@
-const docketsRouter = require("../routes/docketsRouter");
-const quotesRouter = require("../routes/quotesRouter");
-const shipmentsRouter = require("../routes/shipmentsRouter");
-const userRouter = require("../routes/userRouter");
+const docketsRouter = require('../routes/docketsRouter');
+const quotesRouter = require('../routes/quotesRouter');
+const shipmentsRouter = require('../routes/shipmentsRouter');
+const customerRouter = require('../routes/customerRouter');
+const userRouter = require('../routes/userRouter');
 
 const routerLoader = (app) => {
-  app.use("/dockets", docketsRouter);
-  app.use("/quotes", quotesRouter);
-  app.use("/shipments", shipmentsRouter);
-  app.use("/user", userRouter);
+  app.use('/dockets', docketsRouter);
+  app.use('/quotes', quotesRouter);
+  app.use('/shipments', shipmentsRouter);
+  app.use('/user', userRouter);
+  app.use('/customers', customerRouter);
   //default route
-  app.get("*", (req, res) => {
-    res.status(200).send("Alternative Die Cutting Inc. Backend API");
+  app.get('*', (req, res) => {
+    res.status(200).send('Alternative Die Cutting Inc. Backend API');
   });
 };
 

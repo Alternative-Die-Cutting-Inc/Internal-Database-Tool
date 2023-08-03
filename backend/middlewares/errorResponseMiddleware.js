1f4b42de50322d721dff9ba8c2a00d6410850761
Freeassassin <farbodemzadeh@gmail.com>
2023-08-03 12:14:18 -0400

    update error middleware

--- backend/middlewares/errorResponseMiddleware.js
+++ backend/middlewares/errorResponseMiddleware.js
@@ -8,7 +8,6 @@ function errorResponseMiddleware(err, req, res, next) {
     errorMessage = 'This email address has already been used to create an account.';
   } else if (err.errors) {
     console.log(err);
-    // TODO: finish error handling when implementing proper backend validation
     statusCode = 400;
     errorMessage = 'Please provide your full name.';
   } else if (err.message === 'INVALID_EMAIL') {
@@ -17,25 +16,26 @@ function errorResponseMiddleware(err, req, res, next) {
   } else if (err.message === 'INVALID_PASSWORD') {
     statusCode = 400;
     errorMessage = 'Please submit a valid password.';
-  } else if (err.message === 'USER_ALREADY_SIGNED_INTO_SCUNT_DISCORD') {
-    statusCode = 400;
-    errorMessage = 'User has already signed into discord for scunt.';
-  } else if (err.message === 'INVALID_SCUNT_CODE') {
-    statusCode = 400;
-    errorMessage = 'Incorrect Scunt Token.';
   } else if (err.message === 'UNABLE_TO_UPDATE_USER') {
     statusCode = 400;
     errorMessage = 'Error in updating user.';
   } else if (err.message === 'UNAUTHORIZED') {
     statusCode = 403;
     errorMessage = 'Unauthorized';
+  } else if (err.message === 'QUOTE_NOT_FOUND') {
+    statusCode = 404;
+    errorMessage = 'This quote does not exist. Please recheck the quote number.';
+  } else if (err.message === 'UNABLE_TO_GET_QUOTE') {
+    statusCode = 400;
+    errorMessage =
+      'There was an error while trying to retrieve the quote. Please try again and check the quote number.';
   } else {
     statusCode = 500;
     errorMessage = 'whoops we have no idea what happened!?';
   }
   //... for more error messages ...
 
-  res.status(statusCode).send({ errorMessage });
+  res.status(statusCode).send({ errorMessage, code: err?.message });
 }
 
 module.exports = errorResponseMiddleware;

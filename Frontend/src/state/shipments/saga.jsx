ed207c64175442654b0363e2d282ca60c6c611cb
Farbod M <farbodm.dev@gmail.com>
2023-08-23 12:08:30 -0400

    Add shipments state

---
+++ Frontend/src/state/shipments/saga.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createAction } from "@reduxjs/toolkit";
+import { put, call, takeLeading } from "redux-saga/effects";
+import useAxios from "../../hooks/useAxios";
+import {
+  getShipmentsStart,
+  getShipmentsSuccess,
+  getShipmentsFailure,
+  getShipmentStart,
+  getShipmentSuccess,
+  getShipmentFailure,
+  createShipmentStart,
+  createShipmentSuccess,
+  createShipmentFailure,
+  updateShipmentStart,
+  updateShipmentSuccess,
+  updateShipmentFailure,
+} from "./shipmentsSlice";
+
+export const getShipments = createAction("getShipmentsSaga");
+
+export function* getShipmentsSaga() {
+  const { axios } = useAxios();
+
+  try {
+    yield put(getShipmentsStart());
+    const response = yield call(axios.get, "/shipments");
+    yield put(getShipmentsSuccess(response?.data?.shipments));
+  } catch (error) {
+    yield put(getShipmentsFailure(error.response?.data?.errorMessage));
+  }
+}
+
+export const getShipment = createAction("getShipmentSaga");
+
+export function* getShipmentSaga({ payload: { id } }) {
+  const { axios } = useAxios();
+
+  try {
+    yield put(getShipmentStart());
+    const response = yield call(axios.get, `/shipments/${id}`);
+    yield put(getShipmentSuccess(response?.data?.shipment));
+  } catch (error) {
+    yield put(getShipmentFailure(error.response?.data?.errorMessage));
+  }
+}
+
+export const createShipment = createAction("createShipmentSaga");
+
+export function* createShipmentSaga({ payload: shipment }) {
+  const { axios } = useAxios();
+
+  try {
+    yield put(createShipmentStart());
+    const response = yield call(axios.post, "/shipments", { shipment });
+    yield put(createShipmentSuccess(response?.data?.shipment));
+  } catch (error) {
+    yield put(createShipmentFailure(error.response?.data?.errorMessage));
+  }
+}
+
+export const updateShipment = createAction("updateShipmentSaga");
+
+export function* updateShipmentSaga({ payload: { id, fields } }) {
+  const { axios } = useAxios();
+
+  try {
+    yield put(updateShipmentStart());
+    const response = yield call(axios.put, `/shipments/${id}`, { fields });
+    yield put(updateShipmentSuccess(response?.data));
+  } catch (error) {
+    yield put(updateShipmentFailure(error.response?.data?.errorMessage));
+  }
+}
+
+export default function* shipmentsSaga() {
+  yield takeLeading(getShipments.type, getShipmentsSaga);
+  yield takeLeading(getShipment.type, getShipmentSaga);
+  yield takeLeading(createShipment.type, createShipmentSaga);
+  yield takeLeading(updateShipment.type, updateShipmentSaga);
+}
---
+++ Frontend/src/state/shipments/shipmentsSlice.jsx
@@ -0,0 +1,97 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+
+export const initialState = {
+  loading: false,
+  error: null,
+  shipments: [],
+  shipment: null,
+};
+
+const shipmentsSlice = createSlice({
+  name: "shipmentsReducer",
+  initialState,
+  reducers: {
+    getShipmentsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    getShipmentsSuccess: (state, { payload: shipments }) => {
+      state.loading = false;
+      state.error = null;
+      state.shipments = shipments;
+    },
+    getShipmentsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    getShipmentStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    getShipmentSuccess: (state, { payload: shipment }) => {
+      state.loading = false;
+      state.error = null;
+      state.shipment = shipment;
+    },
+    getShipmentFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    createShipmentStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    createShipmentSuccess: (state, { payload: shipment }) => {
+      state.loading = false;
+      state.error = null;
+      state.shipment = shipment;
+    },
+    createShipmentFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    updateShipmentStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateShipmentSuccess: (state, { payload: shipment }) => {
+      state.loading = false;
+      state.error = null;
+      state.shipment = shipment;
+    },
+    updateShipmentFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+  },
+});
+
+export const {
+  getShipmentsStart,
+  getShipmentsSuccess,
+  getShipmentsFailure,
+  getShipmentStart,
+  getShipmentSuccess,
+  getShipmentFailure,
+  createShipmentStart,
+  createShipmentSuccess,
+  createShipmentFailure,
+  updateShipmentStart,
+  updateShipmentSuccess,
+  updateShipmentFailure,
+} = shipmentsSlice.actions;
+
+export default shipmentsSlice.reducer;
+
+export const shipmentsReducerSelector = (state) => state[shipmentsSlice.name];
+
+export const shipmentsSelector = createSelector(
+  shipmentsReducerSelector,
+  ({ shipments, error, loading }) => ({ shipments, error, loading })
+);
+
+export const shipmentSelector = createSelector(
+  shipmentsReducerSelector,
+  ({ shipment, error, loading }) => ({ shipment, error, loading })
+);

91a6cb17bf4bf3d4fa99b16a3d0210aef63929d2
Farbod M <farbodm.dev@gmail.com>
2023-09-10 21:49:07 -0400

    update server port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ mongoLoader(app).then(async () => {
   routerLoader(app);
   app.use(errorResponseMiddleware);
 
-  server.listen(8080, () => {
-    console.log(`Server is running on port: 8080`);
+  server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
   });
 });

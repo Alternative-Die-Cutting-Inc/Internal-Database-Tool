9ebb4037ae4b4c5906c985122271547f921a1a7a
Farbod M <farbodm.dev@gmail.com>
2023-11-10 22:59:33 -0500

    Added editing logic to customer admin tab

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -4,7 +4,11 @@ import { Fragment, useEffect, useState } from "react";
 import { usersSelector } from "../../state/user/userSlice";
 import { getUsers } from "../../state/user/saga";
 import { customersSelector } from "../../state/customers/customerSlice";
-import { getCustomers } from "../../state/customers/saga";
+import {
+  createCustomer,
+  deleteCustomer,
+  getCustomers,
+} from "../../state/customers/saga";
 import { changeRates, getRates } from "../../state/quotes/saga";
 import { ratesSelector } from "../../state/quotes/quoteSlice";
 
@@ -92,6 +96,14 @@ const CustomerTab = () => {
         info: "",
       },
     ],
+    address: {
+      line1: null,
+      line2: null,
+      city: null,
+      province: null,
+      postalCode: null,
+      notes: null,
+    },
   });
 
   useEffect(() => {
@@ -105,10 +117,47 @@ const CustomerTab = () => {
         </header>
         <div className="customer-form-info">
           <div className="form-column" id="customer-form-column1">
-            <input type="text" placeholder="Name" required />
-            <input type="number" placeholder="Premium" required />
-            <input type="text" placeholder="Memo" />
-            <select name="type" id="type">
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  name: event.target.value,
+                });
+              }}
+            />
+            <input
+              type="number"
+              placeholder="Premium"
+              required
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  premium: parseFloat(event.target.value),
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Memo"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  memo: event.target.value,
+                });
+              }}
+            />
+            <select
+              name="type"
+              id="type"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  type: event.target.value,
+                });
+              }}
+            >
               <option value="customer">Customer</option>
               <option value="supplier">Supplier</option>
               <option value="perspective">Perspective</option>
@@ -136,28 +185,157 @@ const CustomerTab = () => {
             </button>
             {newCustomer.contacts.map((contact, index) => (
               <div key={index} className="customer-form-contact">
-                <select name="type" id="type">
+                <select
+                  name="type"
+                  id="type"
+                  value={contact.type}
+                  onChange={(event) => {
+                    setNewCustomer({
+                      ...newCustomer,
+                      contacts: newCustomer.contacts.map((contact, i) => {
+                        if (i == index) {
+                          return {
+                            ...contact,
+                            type: event.target.value,
+                          };
+                        } else return contact;
+                      }),
+                    });
+                  }}
+                >
                   <option value="phone">Phone</option>
                   <option value="email">Email</option>
                   <option value="fax">Fax</option>
                 </select>
-                <input type="text" placeholder="Label" required />
-                <input type="text" placeholder="Info" required />
+                <input
+                  type="text"
+                  placeholder="Label"
+                  required
+                  onChange={(event) => {
+                    setNewCustomer({
+                      ...newCustomer,
+                      contacts: newCustomer.contacts.map((contact, i) => {
+                        if (i == index) {
+                          return {
+                            ...contact,
+                            label: event.target.value,
+                          };
+                        } else return contact;
+                      }),
+                    });
+                  }}
+                />
+                <input
+                  type={contact.type == "email" ? "email" : "text"}
+                  placeholder="Info"
+                  required
+                  onChange={(event) => {
+                    setNewCustomer({
+                      ...newCustomer,
+                      contacts: newCustomer.contacts.map((contact, i) => {
+                        if (i == index) {
+                          return {
+                            ...contact,
+                            info: event.target.value,
+                          };
+                        } else return contact;
+                      }),
+                    });
+                  }}
+                />
               </div>
             ))}
           </div>
           <div className="form-column" id="customer-form-column3">
-            <input type="text" placeholder="Address Line 1" />
-            <input type="text" placeholder="Address Line 2" />
-            <input type="text" placeholder="City" />
+            <input
+              type="text"
+              placeholder="Address Line 1"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    line1: event.target.value,
+                  },
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Address Line 2"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    line2: event.target.value,
+                  },
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="City"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    city: event.target.value,
+                  },
+                });
+              }}
+            />
           </div>
           <div className="form-column" id="customer-form-column4">
-            <input type="text" placeholder="Province" />
-            <input type="text" placeholder="Postal Code" />
-            <input type="text" placeholder="Notes" />
+            <input
+              type="text"
+              placeholder="Province"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    province: event.target.value,
+                  },
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Postal Code"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    postalCode: event.target.value,
+                  },
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Notes"
+              onChange={(event) => {
+                setNewCustomer({
+                  ...newCustomer,
+                  address: {
+                    ...newCustomer.address,
+                    notes: event.target.value,
+                  },
+                });
+              }}
+            />
           </div>
 
-          <input type="submit" value={"Create Customer"} />
+          <input
+            type="submit"
+            value={"Create Customer"}
+            onClick={() => {
+              dispatch(createCustomer({ customer: newCustomer }));
+            }}
+          />
         </div>
       </div>
       <div className="customers-list">
@@ -165,7 +343,7 @@ const CustomerTab = () => {
           <h2>Customers</h2>
         </header>
         {customers.length &&
-          customers.map((customer, index) => (
+          customers.map((customer, index) => ( 
             <div key={index} className="customer-list-item">
               <div className="form-column" id="customer-form-column1">
                 <input type="text" value={customer.name} />
@@ -201,18 +379,53 @@ const CustomerTab = () => {
                 ))}
               </div>
               <div className="form-column" id="customer-form-column3">
-                <input type="text" placeholder="Address Line 1" />
-                <input type="text" placeholder="Address Line 2" />
-                <input type="text" placeholder="City" />
+                <input
+                  type="text"
+                  placeholder="Address Line 1"
+                  value={customer?.address?.line1 || ""}
+                />
+                <input
+                  type="text"
+                  placeholder="Address Line 2"
+                  value={customer?.address?.line2 || ""}
+                />
+                <input
+                  type="text"
+                  placeholder="City"
+                  value={customer?.address?.city || ""}
+                />
               </div>
               <div className="form-column" id="customer-form-column4">
-                <input type="text" placeholder="Province" />
-                <input type="text" placeholder="Postal Code" />
-                <input type="text" placeholder="Notes" />
+                <input
+                  type="text"
+                  placeholder="Province"
+                  value={customer?.address?.province || ""}
+                />
+                <input
+                  type="text"
+                  placeholder="Postal Code"
+                  value={customer?.address?.postalCode || ""}
+                />
+                <input
+                  type="text"
+                  placeholder="Notes"
+                  value={customer?.address?.notes || ""}
+                />
               </div>
               <div className="form-column">
-                <input type="submit" value={"Edit Customer"} />
-                <input type="submit" value={"Delete Customer"} />
+                <input
+                  type="submit"
+                  value={"Edit Customer"}
+                  style={{ backgroundColor: "yellow" }}
+                />
+                <input
+                  type="submit"
+                  value={"Delete Customer"}
+                  style={{ backgroundColor: "red" }}
+                  onClick={() => {
+                    dispatch(deleteCustomer({ id: customer._id }));
+                  }}
+                />
               </div>
             </div>
           ))}
